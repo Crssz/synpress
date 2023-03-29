a826988b72c29600cd47058d22c4acdaeed65b1a
Pai Narawit <narawittriprom@gmail.com>
2023-03-29 17:15:33 +0700

    add extra option

--- commands/metamask.js
+++ commands/metamask.js
@@ -808,11 +808,18 @@ const metamask = {
       );
       await module.exports.confirmSignatureRequest();
     } else {
-      await playwright.waitAndClick(
-        permissionsPageElements.connectButton,
-        notificationPage,
-        { waitForEvent: 'close' },
-      );
+      if (options.dontWaitAfterConnect) {
+        await playwright.waitAndClick(
+          permissionsPageElements.connectButton,
+          notificationPage,
+        );
+      } else {
+        await playwright.waitAndClick(
+          permissionsPageElements.connectButton,
+          notificationPage,
+          { waitForEvent: 'close' },
+        );
+      }
     }
     return true;
   },
